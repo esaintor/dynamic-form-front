0b2e54efa3f1775ad3379b349547016e57ad7395
unknown <esaintor@itzone-sw-790.itzone.local>
2017-08-26 15:40:41 +0800

    json

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// var jsonobject = [];
+// var jsonform = [];
+// var jsonfields = [];
 var onclickclose = function(e) {
     console.log(e);
     $(e.currentTarget.parentElement).remove();
@@ -5,11 +8,11 @@ var onclickclose = function(e) {
 };
 var onclickedit = function(e) {
     console.log(e);
-    var person = prompt("Please enter label name:", );
-    if (person == null || person == "") {
+    var labelText = prompt("Please enter label name:", );
+    if (labelText == null || labelText == "") {
         alert("it is empty");
     } else {
-        $(e.currentTarget.parentElement.children[2]).text(person);
+        $(e.currentTarget.parentElement.children[2]).text(labelText);
     }
     console.log("double click");
 };
@@ -21,6 +24,7 @@ var droppableObject = {
     },
     drop: function(event, ui) {
         if (ui.draggable.context.className.includes("item-draggable")) {
+            console.log(event);
             var target = event.target.children[1];
             var item;
             var div = document.createElement("div");
@@ -35,21 +39,21 @@ var droppableObject = {
             if (ui.draggable.context.children[0].localName == "input" && ui.draggable.context.children[0].type == "text") {
                 item = document.createElement("input");
                 $(item).addClass("form-control");
-                $(item).attr("text");
+                $(item).attr("type", "text");
                 $(div).append(item);
             }
             // if it is email
             if (ui.draggable.context.children[0].localName == "input" && ui.draggable.context.children[0].type == "email") {
                 item = document.createElement("input");
                 $(item).addClass("form-control");
-                $(item).attr("email");
+                $(item).attr("type", "email");
                 $(div).append(item);
             }
             // if it is password
             if (ui.draggable.context.children[0].localName == "input" && ui.draggable.context.children[0].type == "password") {
                 item = document.createElement("input");
                 $(item).addClass("form-control");
-                $(item).attr("password");
+                $(item).attr("type", "password");
                 $(div).append(item);
             }
             // add close button
@@ -82,6 +86,27 @@ $(document).ready(function() {
         $(".form-ui").remove();
     });
 
+    $("#save").click(function() {
+        var forms = [];
+        //console.log($(".form-ui"));
+        $(".form-ui").each(function(index, formElement) {
+            //console.log($(formElement))
+            var label = $(formElement).context.children[0].innerText;
+            var fields = [];
+
+            $($(formElement).context.children[1].children).each(function(index, fieldElement) {
+                //console.log($(fieldElement));
+                var label = $(fieldElement).context.children[2].innerText;
+                var type = $(fieldElement).context.children[3].type;
+                var field = { id: index, label: label, type: type };
+                fields.push(field);
+            });
+            var form = { id: index, label: label, fields: fields };
+            forms.push(form);
+        });
+        console.log(JSON.stringify(forms));
+    });
+
     $(function() {
         $(".sortable").sortable();
         $(".sortable").disableSelection();
@@ -96,7 +121,11 @@ $(document).ready(function() {
             },
             drop: function(event, ui) {
                 if (ui.draggable.context.className.includes("form-draggable")) {
+
                     var main = $(".main-form")[0].children[1];
+                    // build json
+                    // jsonform = { id: "form" + (main.children.length + 1) };
+                    // jsonobject.push(jsonform);
                     var target = event.target.children[1];
                     var div = document.createElement("div");
                     $(div).addClass("form-ui ui-sortable-handle");
